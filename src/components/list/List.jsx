adb1216be7ee05497648340534198a378e3db2c6
John Solomou <solomoufam@yahoo.com>
2022-10-20 09:02:05 -0400

    adding list and list item components

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,5 +1,50 @@
+import {
+  ArrowBackIosOutlined,
+  ArrowForwardIosOutlined,
+} from "@material-ui/icons";
+import { useRef, useState } from "react";
+import ListItem from "../listItem/ListItem";
 import "./list.scss";
 
 export default function List() {
-  return <div className="list">List</div>;
+  const listRef = useRef();
+
+  const handelClick = (direction) => {
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === "left") {
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+    if (direction === "right") {
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    }
+    console.log(distance);
+  };
+
+  return (
+    <div className="list">
+      <span className="listTitle">Continue to watch</span>
+      <div className="wrapper">
+        <ArrowBackIosOutlined
+          className="sliderArrow left"
+          onClick={() => handelClick("left")}
+        />
+        <div className="container" ref={listRef}>
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+        </div>
+        <ArrowForwardIosOutlined
+          className="sliderArrow right"
+          onClick={() => handelClick("right")}
+        />
+      </div>
+    </div>
+  );
 }
