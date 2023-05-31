8715129406ac08d36dec0290e0dfc5f3c6425999
John Solomou <solomoufam@yahoo.com>
2023-05-31 06:43:41 -0400

    fixing url path for login

--- src/authContext/apiCalls.js
+++ src/authContext/apiCalls.js
@@ -1,14 +1,11 @@
 import axios from "axios";
-// import { REACT_APP_SERVER_URL } from "../constants/constant";
+import { REACT_APP_SERVER_URL } from "../constants/constant";
 import { loginStart, loginFailure, loginSuccess } from "./AuthActions";
 
 export const login = async (user, dispatch) => {
   dispatch(loginStart());
   try {
-    const res = await axios.post(
-      process.env.REACT_APP_SERVER_URL + "auth/login",
-      user
-    );
+    const res = await axios.post(REACT_APP_SERVER_URL + "/auth/login", user);
     dispatch(loginSuccess(res.data));
   } catch (error) {
     dispatch(loginFailure());
--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,1 +1,2 @@
-// export const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
+export const REACT_APP_SERVER_URL =
+  "https://john-solomou-netflix-api.onrender.com/api";
